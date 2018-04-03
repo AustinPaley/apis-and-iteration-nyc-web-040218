33652b3c6ed0e6812745aa0e5dfea165c5f3cc92
AustinPaley <austinpaley32@gmail.com>
2018-04-03 16:36:52 -0400

    Completed w/ Bonus

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,23 +1,92 @@
+# require 'rest-client'
+# require 'json'
+# require 'pry'
+#
+# def get_character_movies_from_api(character)
+#   all_characters = RestClient.get("http://www.swapi.co/api/people/")
+#   character_hash = JSON.parse(all_characters)
+#
+#     character_hash["results"].each do |characterHash|
+#       if characterHash["name"] == format_string(character)
+#         films = characterHash["films"]
+#         return films
+#       elsif characterHash["name"] == character.upcase
+#         films = characterHash["films"]
+#         return films
+#       else
+#         return nil
+#       end
+#     end
+# end
+#
+#
+#
+# def show_character_movies(character)
+#   json_apis = get_character_movies_from_api(character)
+#   all_titles = []
+#
+#   if json_apis == nil
+#     return "ERROR - enter a real character."
+#   end
+#
+#   json_apis.each do |json_api|
+#     film_hash = json_api_to_hash(json_api)
+#     all_titles << film_hash["title"]
+#   end
+#
+#   all_titles
+# end
+#
+#
+# def json_api_to_hash(json_api)
+#   films = RestClient.get(json_api)
+#   JSON.parse(films)
+# end
+#
+# def format_string(string)
+#   formatted_string = ""
+#
+#   names = string.split(" ")
+#   names.each_with_index do |name, index|
+#     if index == names.length - 1
+#       formatted_string += name[0].capitalize() + name.slice(1, name.length)
+#     else
+#       formatted_string += name[0].capitalize() + name.slice(1, name.length) + " "
+#     end
+#   end
+#
+#     formatted_string
+# end
+
 require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
 
-    character_hash["results"].each do |characterHash|
-      if characterHash["name"] == format_string(character)
-        films = characterHash["films"]
-        return films
-      elsif characterHash["name"] == character.upcase
-        films = characterHash["films"]
-        return films
-      else
-        return nil
+def get_character_movies_from_api(character)
+  counter = 1
+
+  while counter < 10
+    all_characters = RestClient.get("http://www.swapi.co/api/people/?page=#{counter}")
+    character_hash = JSON.parse(all_characters)
+
+      character_hash["results"].each do |characterHash|
+        if characterHash["name"] == format_string(character)
+          films = characterHash["films"]
+          return films
+        elsif characterHash["name"] == character.upcase
+          films = characterHash["films"]
+          return films
+        end
       end
-    end
+      
+      counter += 1
+  end
+
+  return nil
 end
+
+
 
 def show_character_movies(character)
   json_apis = get_character_movies_from_api(character)
@@ -34,6 +103,7 @@ def show_character_movies(character)
 
   all_titles
 end
+
 
 def json_api_to_hash(json_api)
   films = RestClient.get(json_api)
